96d988263415a1afc784a59a3a68c4be4392cec0
MauricioYPS <yepes060@gmail.com>
2026-01-10 21:58:38 -0500

    pequeños cambios

--- MOVILCOFrontend/src/Pages/DataWorkflow.jsx
+++ MOVILCOFrontend/src/Pages/DataWorkflow.jsx
@@ -310,10 +310,7 @@ const DataWorkflow = () => {
       <div className="flex-1 flex flex-col min-w-0 overflow-hidden">
         <header className="bg-white z-10 h-16 flex items-center justify-between px-6 lg:px-10 ">
           <div className="flex items-center gap-4">
-            <button onClick={() => setSidebarOpen(true)} className="lg:hidden text-gray-500">
-              <Icon name="menu" size={24} />
-            </button>
-            <h1 className="text-lg font-bold text-slate-800">Centro de Procesamiento ETL</h1>
+
           </div>
           <div className="flex items-center gap-4">
             <div className="flex items-center gap-2 bg-slate-50 px-4 py-2 rounded-lg border border-gray-200">
